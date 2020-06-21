ed1177fd8f0d5c9ef5cff4a62f360cf0ea526c94
작심1일 <sgysnc@daum.net>
2020-06-21 13:59:24 +0000

    C언어 기초 1강 & 2강

---
+++ C_Basic1/Hello_World_ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World!");
+	
+	return 0;
+}
+
+/* 	Hello, World! 엔 특별한 유래가 있다.
+	1978년 C와 UNIX를 개발한 것으로 유명한 데니스 리치와 브라이언 커니핸
+	두 사람이 쓴 "The C Programming Language" 교재의 첫 예제가
+	바로 화면에 Hello, World 를 출력하는 것이었는데,
+	이 예제가 유명해지면서 모든 프로그래밍 언어의 시작 예제로 굳어지게 되었다.
+*/
---
+++ C_Basic1/Hello_World_ex4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("****\n");
+	printf("*****\n");
+	
+	return 0;
+}
+
+/*
+	*
+	**
+	***
+	****
+	*****
+	
+	과 같은 모양이 출력되도록 코드 작성.(이스퀘이프 시퀀스 이용)
+*/
