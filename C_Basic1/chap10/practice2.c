2c9e4d93b090d15f54a64b0bcf5ee13026f4dc65
작심1일 <sgysnc@daum.net>
2020-06-28 13:54:25 +0000

    10강연습

---
+++ C_Basic1/chap10/practice2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int arr1[3][3];
+	int arr2[3][3];
+	int arr3[3][3];
+	
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			scanf("%d", &arr1[i][j]);
+		}
+	}
+	
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			scanf("%d", &arr2[i][j]);
+		}
+	}
+
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			arr3[i][j] = arr1[i][j] + arr2[i][j];
+			printf("%d ", arr3[i][j]);
+		}
+		printf("\n");
+	}
+	
+  return 0;
+}
